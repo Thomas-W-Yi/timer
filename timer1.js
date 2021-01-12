6acdb57385967d1415bda17bdc86b61bee3a7aaa
Your Name <thomas.w.yee@gmail.com>
2021-01-12 15:31:55 -0500

    timer

---
+++ timer1.js
@@ -0,0 +1,17 @@
+const log = console.log;
+const timer = (arr) => {
+  let x, y, times;
+  [x, y, ...times] = arr;
+  let sorted = times.sort((a, b) => {
+    return a - b;
+  });
+  for (let i = 0; i < sorted.length; i++) {
+    log(sorted[i]);
+    sorted[i] <= 0 || !Number(sorted[i]) ? null :
+      setTimeout(() => {
+        process.stdout.write('.');
+      }, sorted[i] * 1000);
+  }
+};
+
+timer(process.argv);
